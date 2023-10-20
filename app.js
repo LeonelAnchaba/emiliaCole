abde3a3ad0759e1020a3cf0960503cf1a7779e15
LeonelAnchaba <leonelanchaba@gmail.com>
2023-10-20 00:12:43 -0300

    Resuelta toda la parte del HTML de Emilia Cole

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ app.get("/", (req, res) =>{
     res.sendFile(path.join(__dirname, "/views/index.html"));
 })
 
+app.get("/music", (req, res) =>{
+    res.sendFile(path.join(__dirname, "/views/music.html"));
+})
+
+app.get("/about", (req, res) =>{
+    res.sendFile(path.join(__dirname, "/views/about.html"));
+})
+
+app.get("/contact", (req, res) =>{
+    res.sendFile(path.join(__dirname, "/views/contact.html"));
+})
+
 app.use(express.static("public"))
 
 app.listen (3000, ()=>{
